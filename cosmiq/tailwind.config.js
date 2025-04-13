e24b699113de7c3922915f21b1eacd2e87407769
Ethan <ethanokamura3@gmail.com>
2025-04-12 17:08:11 -0700

    minor changes to workflow configuration

--- cosmiq/tailwind.config.js
+++ cosmiq/tailwind.config.js
@@ -1,77 +1,75 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin");
 export default {
-  mode: 'jit',
-	content: ['./src/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'class',
-	theme: {
-		extend: {
+  mode: "jit",
+  content: ["./src/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
       colors: {
-        'text': '#e1e6f0',
-        'text2': '#969eb0',
-        'text3': '#666e7d',
-        'background': '#10141a',
-        'surface': '#61AFEF',
-        'accent': '#151b24',
+        text: "#e1e6f0",
+        text2: "#969eb0",
+        text3: "#666e7d",
+        background: "#10141a",
+        surface: "#61AFEF",
+        accent: "#151b24",
         green: {
-          100:  '#98C379',
+          100: "#98C379",
         },
         red: {
-          100: '#E06C75',
+          100: "#E06C75",
         },
       },
       fontFamily: {
-        grotesk: 'Hanken Grotesk',
-        rubik: 'Rubik',
+        grotesk: "Hanken Grotesk",
+        rubik: "Rubik",
       },
       gridTemplateRows: {
-        home: [
-          'auto 1fr auto'
-        ]
+        home: ["auto 1fr auto"],
       },
       animation: {
         typing: "typing 2s steps(50)",
-        blink: "blink 0.75s infinite"
+        blink: "blink 0.75s infinite",
       },
       keyframes: {
         typing: {
           from: {
             width: "0",
-            visibility: "hidden"
+            visibility: "hidden",
           },
           to: {
-            width: "100%"
-          }
+            width: "100%",
+          },
         },
         blink: {
           "50%": {
-            opacity: "0%"
+            opacity: "0%",
           },
           "100%": {
-            opacity: "100%"
-          }
-        }
-      }
+            opacity: "100%",
+          },
+        },
+      },
     },
-	},
-	plugins: [
+  },
+  plugins: [
     plugin(function ({ matchUtilities, theme }) {
       matchUtilities(
         {
-          'text-shadow': (value) => ({
+          "text-shadow": (value) => ({
             textShadow: value,
           }),
-          'animate-delay': (value) => ({
+          "animate-delay": (value) => ({
             animationDelay: value,
           }),
-          'opacity': (value) => ({
+          opacity: (value) => ({
             opacity: value,
           }),
         },
-        { values: theme('textShadow') },
-        { values: theme('transitionDelay') },
-        { values: theme('opacity') }
-      )
+        { values: theme("textShadow") },
+        { values: theme("transitionDelay") },
+        { values: theme("opacity") },
+      );
     }),
   ],
-}
+};
