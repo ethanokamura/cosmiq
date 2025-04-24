b0b7dd32a8b988a6a335dc50b96f7532ac7efa6f
Ethan <ethanokamura3@gmail.com>
2025-04-23 21:31:04 -0700

    updated macos window

--- cosmiq/src-tauri/src/lib.rs
+++ cosmiq/src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 use reqwest;
 use serde::Deserialize;
 use tauri::command;
+use tauri::Manager;
 
 #[derive(Debug, Deserialize)]
 pub struct GenerateQuizInput {
@@ -62,6 +63,33 @@ async fn generate_quiz(input: GenerateQuizInput) -> Result<String, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+          if let Some(window) = app.get_webview_window("main") {
+              // set transparent title bar only when building for macOS
+              #[cfg(target_os = "macos")]
+              window.set_title_bar_style(tauri::TitleBarStyle::Transparent).unwrap();
+
+              // set background color only when building for macOS
+              #[cfg(target_os = "macos")]
+              {
+                  use cocoa::appkit::{NSColor, NSWindow};
+                  use cocoa::base::{id, nil};
+
+                  let ns_window = window.ns_window().unwrap() as id;
+                  unsafe {
+                      let bg_color = NSColor::colorWithRed_green_blue_alpha_(
+                          nil,
+                          28.0 / 255.0,
+                          35.0 / 255.0,
+                          46.0 / 255.0,
+                          1.0,
+                      );
+                      ns_window.setBackgroundColor_(bg_color);
+                  }
+              }
+          }
+          Ok(())
+        })
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![generate_summary, generate_quiz])
